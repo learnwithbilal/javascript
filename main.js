4ea23e88739082dfb9f0cfe5c9972475fff4645d
learnwithbilal <learn.with.bilal@gmail.com>
2021-10-13 18:10:56 +0300

    add javascript

--- main.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  document.querySelector("select").onchange = function () {
-    document.querySelector("h1").style.color = this.value;
-  };
-});
